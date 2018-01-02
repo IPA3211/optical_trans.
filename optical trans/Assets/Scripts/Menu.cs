db2f97ad363f607e8bbb35468b4bf8f919428aec
there <uioo1@naver.com>
2018-01-03 02:09:17 +0900

    restart는 되는데 브금은 해결못함

--- optical trans/Assets/Scripts/Menu.cs
+++ optical trans/Assets/Scripts/Menu.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class Menu : MonoBehaviour {
-
+	
+	public static string scenename;
     public GameObject canvas;
     public bool paused = false;
 	// Use this for initialization
 	void Start () {
+		scenename = SceneManager.GetActiveScene ().name;
 	}
 	
 	// Update is called once per frame
@@ -23,4 +28,9 @@ public class Menu : MonoBehaviour {
         canvas.SetActive(paused);
         Time.timeScale = Time.timeScale == 0 ? 1 : 0;
     }
+
+	public void Restart() {
+		SceneManager.LoadScene (scenename, LoadSceneMode.Single);
+		Onoff ();
+	}
 }
