e662f02c700ef1c91b42c24c3a65ef670eb10cc6
ipa32 <kygsm2003@naver.com>
2018-01-15 23:59:09 +0900

    대포 만들고 있었음

---
+++ optical trans/Assets/Scripts/BulletCannon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCannon : MonoBehaviour {
+    public float BulletSpeed = 0;
+    float angle = 0;
+    BoxCollider2D box;
+    
+    public float DestroyTime;
+    
+    // Use this for initialization
+    void Start () {
+        Destroy(gameObject, DestroyTime);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += new Vector3(BulletSpeed * Time.deltaTime, 0, 0);
+    }
+}
