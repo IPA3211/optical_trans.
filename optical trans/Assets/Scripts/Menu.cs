45d8f92e0e36a8a5ba0afde42cea99e92feb7e26
ipa32 <kygsm2003@naver.com>
2018-01-19 19:15:14 +0900

    이제 시작할떄 해상도 설정을 하지 않고 바로 시작, esc 옵션 기능에서 해상도 조정 가능(Defult: 1920x 1080 fullscreen), 소리는 아직

--- optical trans/Assets/Scripts/Menu.cs
+++ optical trans/Assets/Scripts/Menu.cs
@@ -1,33 +1,66 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 
 public class Menu : MonoBehaviour {
-	
-	public static string scenename;
-    public GameObject canvas;
+
+    public static string scenename;
+    public GameObject pausedMenu;
+    public GameObject optionMenu;
+    public GameObject soundSlider;
+    public GameObject soundInputField;
+    public GameObject dropDown;
+    public GameObject windowToggle;
     public bool paused = false;
+    public bool option = false;
     public static bool loading;
-	// Use this for initialization
-	void Start () {
-		scenename = SceneManager.GetActiveScene ().name;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    
+    // Use this for initialization
+
+    void Awake()
+    {
+        LoadOption();
+    }
+
+    void Start() {
+        scenename = SceneManager.GetActiveScene().name;
+        soundSlider.GetComponent<Slider>().onValueChanged.AddListener(delegate { WhanChangeValue(); });
+        soundInputField.GetComponent<InputField>().onEndEdit.AddListener(delegate { WhanChangeText(); });
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Onoff();
+            if (option == true)
+            {
+                OptionOnOff();
+                pausedMenu.SetActive(paused);
+            }
         }
     }
 
+
+
     public void Onoff() {
         paused = !paused;
-        canvas.SetActive(paused);
+        pausedMenu.SetActive(paused);
         Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    }
+    public void OptionOnOff() {
+        option = !option;
+        pausedMenu.SetActive(!option);
+        optionMenu.SetActive(option);
     }
 
     public void OnOffWithOutCanvas() {
@@ -35,13 +68,10 @@ public class Menu : MonoBehaviour {
         Time.timeScale = Time.timeScale == 0 ? 1 : 0;
     }
 
-	public void Restart() {
-        loading = false;
-        StartCoroutine("myYield");
-    }
     IEnumerator myYield()
     {
-        if (paused) {
+        if (paused)
+        {
             Onoff();
         }
         GameObject.Find("Main Camera").GetComponent<GlitchEffect>().enabled = true;
@@ -57,4 +87,104 @@ public class Menu : MonoBehaviour {
         //gameObject.GetComponent<ChangePos>().Gun = GameObject.Find("P_Gun");
     }
     
+    public void Restart() {
+        loading = false;
+        StartCoroutine("myYield");
+    }
+
+    public void SaveOption() {
+        Debug.Log("Save Option");
+        FileStream stream = new FileStream(Application.persistentDataPath + "/option.sav", FileMode.Create);
+        BinaryWriter w = new BinaryWriter(stream);
+
+        w.Write(dropDown.GetComponent<Dropdown>().value);
+        w.Write((int)(soundSlider.GetComponent<Slider>().value * 100 + 0.1));
+        w.Write(windowToggle.GetComponent<Toggle>().isOn);
+
+        w.Close();
+        LoadOption();
+    }
+
+    public void LoadOption() {
+        Debug.Log("Load Option");
+        if (File.Exists(Application.persistentDataPath + "/option.sav"))
+        {
+            FileStream stream = new FileStream(Application.persistentDataPath + "/option.sav", FileMode.Open);
+            BinaryReader r = new BinaryReader(stream);
+
+            dropDown.GetComponent<Dropdown>().value = r.ReadInt32();
+            soundInputField.GetComponent<InputField>().text = r.ReadInt32().ToString();
+            WhanChangeText();
+            windowToggle.GetComponent<Toggle>().isOn = r.ReadBoolean();
+
+            r.Close();
+        }
+        else {
+            Debug.Log("Don't ");
+            dropDown.GetComponent<Dropdown>().value = 0;
+            soundInputField.GetComponent<InputField>().text = "100";
+            windowToggle.GetComponent<Toggle>().isOn = false;
+
+            SaveOption();
+        }
+
+        ChangeResolution();
+    }
+
+    public void WhanChangeValue() {
+        soundInputField.GetComponent<InputField>().text = ((int)(soundSlider.GetComponent<Slider>().value * 100 + 0.1)).ToString();
+    }
+
+    public void WhanChangeText()
+    {
+        if (soundInputField.GetComponent<InputField>().text == null)
+            soundInputField.GetComponent<InputField>().text = "0";
+        soundSlider.GetComponent<Slider>().value = (float)(Int32.Parse(soundInputField.GetComponent<InputField>().text)) / 100;
+    }
+
+    public void ChangeResolution() {
+        switch (dropDown.GetComponent<Dropdown>().value)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, !windowToggle.GetComponent<Toggle>().isOn);
+                break;
+            case 1:
+                Screen.SetResolution(1600, 900, !windowToggle.GetComponent<Toggle>().isOn);
+                break;
+            case 2:
+                Screen.SetResolution(1280, 720, !windowToggle.GetComponent<Toggle>().isOn);
+                break;
+            case 3:
+                Screen.SetResolution(1152, 648, !windowToggle.GetComponent<Toggle>().isOn);
+                break;
+            case 4:
+                Screen.SetResolution(1024, 576, !windowToggle.GetComponent<Toggle>().isOn);
+                break;
+            case 5:
+                Screen.SetResolution(960, 540, !windowToggle.GetComponent<Toggle>().isOn);
+                break;
+        }
+    }
 }
+public struct OptionData {
+    public int Sound;
+    public int Resolution;
+    public bool Window;
+}
+
+    /*
+[Serializable]
+public class OptionData
+{
+    public int Sound;
+    public int Resolution;
+    public bool Window;
+
+    public OptionData(int sound, int resolution, bool window)
+    {
+        Sound = sound;
+        Resolution = resolution;
+        Window = window;
+    }
+}
+*/
