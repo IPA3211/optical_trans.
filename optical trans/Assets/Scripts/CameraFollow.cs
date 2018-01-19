a248b48de5d3c4e3e0ebe2c440180140a2455455
there <uioo1@naver.com>
2018-01-19 19:51:08 +0900

    내가 만든맵을 testmap 씬에다 옮겨놈 근데 너가 asd에서 바꾼건 니껄로 받아야되니까 너가 testmap씬을 지우고 받아야 잘될듯

--- optical trans/Assets/Scripts/CameraFollow.cs
+++ optical trans/Assets/Scripts/CameraFollow.cs
@@ -11,6 +11,10 @@ public class CameraFollow : MonoBehaviour {
 
 	public GameObject player;
 
+	public bool bounds;
+	public Vector3 minCameraPos;
+	public Vector3 maxCameraPos;
+
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
@@ -25,26 +29,17 @@ public class CameraFollow : MonoBehaviour {
 	void FixedUpdate()
 	{
 		
-		if(player.transform.localPosition.x > 0 && player.transform.localPosition.x < 67.4 && player.transform.localPosition.y > 0  && player.transform.localPosition.y < 38.3)	//야매 변수값 개이득 ^^
+
+		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+		transform.position = new Vector3 (posX, posY, transform.position.z);
+	
+		if (bounds) 
 		{
-			float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-			float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
-			transform.position = new Vector3 (posX, posY, transform.position.z);
-			Debug.Log ("x : " + posX + ", y : " + posY);
-		}
-		else if(player.transform.localPosition.x > 0 && player.transform.localPosition.x < 67.4)
-		{
-			float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-			//float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
-			transform.position = new Vector3 (posX, transform.position.y, transform.position.z);
-			Debug.Log ("only x");
-		}
-		else if(player.transform.localPosition.y > 0 && player.transform.localPosition.y < 38.3)
-		{
-			//float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-			float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
-			transform.position = new Vector3 (transform.position.x, posY, transform.position.z);
-			Debug.Log ("only y");
+			transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x), 
+				Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y), 
+				Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
 		}
 			
 	}
