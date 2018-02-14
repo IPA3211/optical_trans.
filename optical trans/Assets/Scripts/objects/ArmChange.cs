dbd93fa30c164fcf8c77ffa9a464c3cb5973bbdb
there <uioo1@naver.com>
2018-02-14 16:40:17 +0900

    메뉴 assign안되어 있던거 다 하고 e버튼 누르는거 조금 크게하고 호밍미사일 애니메이션을 넣었는데 티 안나서 바꿔야되고 너가 그 enemy좀 잘 잘라주라 이상하게 드릅게 안잘리네

---
+++ optical trans/Assets/Scripts/objects/ArmChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmChange : MonoBehaviour {
+	public Sprite currentSprite;
+	public Sprite nextsprite;
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+		spriteRenderer.sprite = currentSprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
