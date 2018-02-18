a9ea978fb07799ca42f17a9a3cf8203b5993b86c
there <uioo1@naver.com>
2018-02-18 20:03:36 +0900

    터렛 총알을 동그랗게 하고 호밍미사일이 터짐 어예~~!!! 그리고 enemy제대로 자름 오예

--- optical trans/Assets/Scripts/homingMissile.cs
+++ optical trans/Assets/Scripts/homingMissile.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class homingMissile : MonoBehaviour
 {
-    public float speed = 5;
-    public float rotatingSpeed = 200;
+    public float speed = 5f;
+    public float rotatingSpeed = 200f;
     GameObject target;
     Rigidbody2D rb;
+	Vector2 point2Target;
 
     void Start()
     {
@@ -17,35 +18,45 @@ public class homingMissile : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 point2Target = (Vector2)transform.position - (Vector2)target.transform.position;
-        point2Target.Normalize();
-        float value = Vector3.Cross(point2Target, transform.right).z;
-        /*
-        if (value > 0) {
-
-                rb.angularVelocity = rotatingSpeed;
-        } else if (value < 0)
-                rb.angularVelocity = -rotatingSpeed;
+		point2Target = (Vector2)transform.position - (Vector2)target.transform.position;
+		point2Target.Normalize ();
+		float value = Vector3.Cross (point2Target, transform.right).z;
+		/*
+       	if (value > 0) {
+               rb.angularVelocity = rotatingSpeed;
+        } 
+        else if (value < 0)
+        	rb.angularVelocity = -rotatingSpeed;
         else
-                rotatingSpeed = 0;
-*/
-        rb.angularVelocity = rotatingSpeed * value;
-        rb.velocity = transform.right * speed;
-        
-    }
+           	rotatingSpeed = 0;
+		*/
+		rb.angularVelocity = rotatingSpeed * value;
+		rb.velocity = transform.right * speed;
+	} 
+
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.GetComponent<ObjectOption>() != null)
         {
             if (other.GetComponent<ObjectOption>().canDestroyByBigEnemy)
-            {
+            {;
                 Destroy(other.gameObject);
             }
             if (other.GetComponent<ObjectOption>().canHit)
             {
-                Destroy(gameObject);
+				gameObject.transform.rotation = new Quaternion (0f, 0f, 0f, 0f); 
+				gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+				speed = 0f;
+				rotatingSpeed = 0f;
+				gameObject.GetComponent<Animator> ().Play ("explosion");
             }
         }
     }
+
+	void Exploded()
+	{
+		Destroy (gameObject);
+	}
+		
 }
 
