374258280d6c92730aa2e8eeda798342584a44c3
ipa32 <kygsm2003@naver.com>
2018-02-21 16:36:23 +0900

    빛효과 약간 넣고 총알 빨르기 증가 및 총알 생존시간 감소

--- optical trans/Assets/Scripts/homingMissile.cs
+++ optical trans/Assets/Scripts/homingMissile.cs
@@ -8,16 +8,32 @@ public class homingMissile : MonoBehaviour
     GameObject target;
     Rigidbody2D rb;
 	Vector2 point2Target;
+    public Color blue, red;
+    Light light;
+    SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player");
+        light = GetComponentInChildren<Light>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (light != null)
+        {
+            if (spriteRenderer.sprite.name.Equals("enemy_9"))
+            {
+                light.color = blue;
+            }
+            else
+            {
+                light.color = red;
+            }
+        }
 		point2Target = (Vector2)transform.position - (Vector2)target.transform.position;
 		point2Target.Normalize ();
 		float value = Vector3.Cross (point2Target, transform.right).z;
@@ -32,14 +48,14 @@ public class homingMissile : MonoBehaviour
 		*/
 		rb.angularVelocity = rotatingSpeed * value;
 		rb.velocity = transform.right * speed;
-	} 
+	}
 
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.GetComponent<ObjectOption>() != null)
         {
             if (other.GetComponent<ObjectOption>().canDestroyByBigEnemy)
-            {;
+            {
                 Destroy(other.gameObject);
             }
             if (other.GetComponent<ObjectOption>().canHit)
@@ -48,6 +64,7 @@ public class homingMissile : MonoBehaviour
 				gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
 				speed = 0f;
 				rotatingSpeed = 0f;
+                Destroy(light);
 				gameObject.GetComponent<Animator> ().Play ("explosion");
             }
         }
